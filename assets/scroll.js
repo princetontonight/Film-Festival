499ce12959325f55d1c5d3dff18c13e5b5fa1ab5
ramonayala <ra4@princeton.edu>
2017-07-31 18:47:32 -0700

    First Commit

---
+++ assets/scroll.js
@@ -0,0 +1,12 @@
+ $('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+    console.log('target');
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - $('header').height()
+        }, 1000);
+    };
+
+});
