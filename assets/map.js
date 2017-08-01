9c0fd42fbb16ef660bdd21df08083496442ce210
Kevin Hou <kevin.ch.hou@gmail.com>
2017-07-31 22:49:26 -0400

    Minor code cleanup and CSS changes

--- assets/map.js
+++ assets/map.js
@@ -1,39 +1,39 @@
 function initialize() {
-	var map;
-	var bounds = new google.maps.LatLngBounds();
-	var mapOptions = {
-		mapTypeId: 'roadmap'
-	};
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
 
-	// Display map
-	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	map.setTilt(45);
+    // Display map
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
 
-	// Markers
-	var markers = [
-	['McCarter Theater, Princeton'],
-	['Lewis Center for the Arts'],
-	['LOT 22 Princeton']
-	];
+    // Markers
+    var markers = [
+    ['McCarter Theater, Princeton'],
+    ['Lewis Center for the Arts'],
+    ['LOT 22 Princeton']
+    ];
 
-	var marker, i, geocoder = new google.maps.Geocoder();
+    var marker, i, geocoder = new google.maps.Geocoder();
 
-	// Loop through array and place markers
-	for(i = 0; i < markers.length; i++) {
-		var address = markers[i][0];
-		geocoder.geocode({'address' : address}, function(results, status) {
-			if (status === 'OK') {
-				map.setCenter(results[0].geometry.location);
-				marker = new google.maps.Marker({
-					map: map,
-					position :results[0].geometry.location
-				});
-			} else {
+    // Loop through array and place markers
+    for(i = 0; i < markers.length; i++) {
+        var address = markers[i][0];
+        geocoder.geocode({'address' : address}, function(results, status) {
+            if (status === 'OK') {
+                map.setCenter(results[0].geometry.location);
+                marker = new google.maps.Marker({
+                    map: map,
+                    position :results[0].geometry.location
+                });
+            } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
-		});
+        });
 
-    	map.fitBounds(bounds);
+        map.fitBounds(bounds);
     }
 
     var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
