aeab1774a1e8a2f2254c4283f935db5bd006df2e
Kevin Hou <kevin.ch.hou@gmail.com>
2017-08-27 13:40:35 -0700

    Mobile optimization for venue section

--- scripts/submission.js
+++ scripts/submission.js
@@ -5,3 +5,7 @@ function submitFilm() {
 function clickedRules() {
     window.open('PUFF2017Rules.pdf', '_blank');
 }
+
+function clickedLocation() {
+    window.open('https://www.google.com/maps/place/East+Pyne+Hall,+Princeton,+NJ+08542/@40.3486338,-74.6605951,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3e6c14325f953:0x6e10edd190d47787!8m2!3d40.3487701!4d-74.6584686', '_blank');
+}
