7fadbe99b9dd6b6aae768486166e24064ef84cfc
Kevin Hou <kevin.ch.hou@gmail.com>
2017-09-18 20:56:35 -0400

    Added date to about section. Update location to Princeton

--- scripts/submission.js
+++ scripts/submission.js
@@ -7,5 +7,5 @@ function clickedRules() {
 }
 
 function clickedLocation() {
-    window.open('https://www.google.com/maps/place/East+Pyne+Hall,+Princeton,+NJ+08542/@40.3486338,-74.6605951,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3e6c14325f953:0x6e10edd190d47787!8m2!3d40.3487701!4d-74.6584686', '_blank');
+    window.open('https://www.google.com/maps/place/Princeton+University/@40.3439888,-74.6536368,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3e6d8cd98b6e9:0x2ba7ed6fa90024f!8m2!3d40.3439888!4d-74.6514481', '_blank');
 }
