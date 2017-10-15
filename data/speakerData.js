d80e979c9e9383cc3e041ce44fb702f055393c8d
Kevin Hou <kevin.ch.hou@gmail.com>
2017-10-14 22:32:13 -0400

    Added in tile-layout-web library

---
+++ data/speakerData.js
@@ -0,0 +1,19 @@
+var gridData = {
+    columns: 4,
+    textColor: "white",
+    openNewWindow: true,
+    data: [
+        {
+            title: "Title",
+            subtitle: "Subtitle",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/test-image.jpg",
+            link: "",
+            size: "1",
+            category: {
+                label: "Producer",
+                color: "orange"
+            }
+        },
+    ]
+}
