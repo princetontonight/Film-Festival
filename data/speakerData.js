56ad247f4ccb0422135a5e48da6ef9741590407f
ramonayala <ra4@princeton.edu>
2017-10-25 16:34:24 -0400

    Added Speakers and Sponsors

--- data/speakerData.js
+++ data/speakerData.js
@@ -4,15 +4,135 @@ var gridData = {
     openNewWindow: true,
     data: [
         {
-            title: "Title",
-            subtitle: "Subtitle",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
-            image: "./assets/test-image.jpg",
-            link: "",
+            title: "Jay Stern",
+            subtitle: "Horrible Bosses, Rush Hour",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/jay.jpg",
+    		//link: "",
             size: "1",
             category: {
                 label: "Producer",
                 color: "orange"
+            }
+        },
+        {
+            title: "Renew Group",
+            subtitle: "In Defense of Art House Cinema",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/gardentheater.jpg",
+    		//link: "",
+            size: "1",
+            category: {
+                label: "Nonprofit",
+                color: "blue"
+            }
+        },
+        {
+            title: "Vicki Horwitz",
+            subtitle: "The Ellen Degeneres Show",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/ellen.jpg",
+    		//link: "",
+            size: "1",
+            category: {
+                label: "Executive Producer",
+                color: "red"
+            }
+        },
+        {
+            title: "Geoff Rich",
+            subtitle: "Avenue Q, The New Group",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/geoff.jpg",
+    		//link: "",
+            size: "1",
+            category: {
+                label: "Producer, Executive Director",
+                color: "Green"
+            }
+        },
+        {
+            title: "Jack Lechner",
+            subtitle: "Columbia University",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/jack.jpg",
+    		//link: "",
+            size: "1",
+            category: {
+                label: "Producer",
+                color: "orange"
+            }
+        },
+        {
+            title: "Sandra Luckow",
+            subtitle: "Barnard College",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/sandra.jpg",
+    		//link: "",
+            size: "1",
+            category: {
+                label: "Producer",
+                color: "orange"
+            }
+        },
+        {
+            title: "Natasha Kalimada",
+            subtitle: "Cirque du Soleil, Actress, Dancer",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/natasha.jpg",
+    		//link: "",
+            size: "1",
+            category: {
+                label: "Performer",
+                color: "purple"
+            }
+        },
+        {
+            title: "Chris Ozminkowski",
+            subtitle: "BBC, AMC",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/chris.jpg",
+            //link: "",
+            size: "1",
+            category: {
+                label: "Former Executive",
+                color: "silver"
+            }
+        },
+        {
+            title: "Nate Johnson",
+            subtitle: "Drift Studios",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/nate.png",
+            //link: "",
+            size: "1",
+            category: {
+                label: "Photographer, Entrepreneur",
+                color: "yellow"
+            }
+        },
+        {
+            title: "Tony Marion",
+            subtitle: "Broadway, Groundhog Day",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/tony.jpg",
+            //link: "",
+            size: "1",
+            category: {
+                label: "Producer",
+                color: "orange"
+            }
+        },
+        {
+            title: "Yaara Sumeruk",
+            subtitle: "Screenwriting",
+            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor neque, rhoncus id rhoncus quis, vestibulum non justo.",
+            image: "./assets/yaara.jpg",
+            //link: "",
+            size: "1",
+            category: {
+                label: "Professor",
+                color: "navy"
             }
         },
     ]
